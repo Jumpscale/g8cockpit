59b95e753a5ecfdb1e1d00179e4f6d0a60066071
christophe de Carvalho Pereira Martincs <christophe.dcpm@gmail.com>
2016-03-09 17:36:20 +0400

    try to discover service first before asking repo path

--- scripts/portforwards.py
+++ scripts/portforwards.py
@@ -15,22 +15,30 @@ def printInfo(msg):
 
 
 @click.command()
-@click.option('--repo', help='path to the ays repo that contains cockpitconfig service', prompt="Please enter the path to the ays repo that contains cockpitconfig service")
+@click.option('--repo', help='path to the ays repo that contains cockpitconfig service')
 def creates_portforwards(repo):
-    if not j.sal.fs.exists(repo):
-        printErr("%s doesn't exists" % repo)
-        return 1
-    if not j.sal.fs.isDir(repo):
-        printErr("%s is not a directory" % reop)
-        return 1
+    if repo is not None:
+        j.sal.fs.changeDir(repo)
+        j.atyourservice.basepath = repo
 
-    j.sal.fs.changeDir(repo)
+    cockpit_cfg = None
+    cockpit_cfg = j.atyourservice.getService(role='cockpitconfig', instance='main', die=False)
+    count = 0
+    if cockpit_cfg is None and count < 3:
+        while cockpit_cfg is None:
+            import ipdb; ipdb.set_trace()
+            printErr("Can't find service cockpitconfig, are you in the cockpit ays repo ?")
+            repo = j.tools.console.askString("Please enter the path to the ays repo that contains cockpitconfig service", defaultparam='', regex=None, retry=2)
+            if not j.sal.fs.exists(repo):
+                contnue
+            j.sal.fs.changeDir(repo)
+            j.atyourservice.basepath = repo
+            cockpit_cfg = j.atyourservice.getService(role='cockpitconfig', instance='main', die=False)
 
     cuisine = j.tools.cuisine.local
     if not cuisine.command_check('autossh'):
         cuisine.package.install('autossh')
 
-    cockpit_cfg = j.atyourservice.getService(role='cockpitconfig', instance='main')
     remote_address = cockpit_cfg.hrd.getStr('node.addr')
     remote_connection_port = cockpit_cfg.hrd.getStr('ssh.port')
     ports = {
