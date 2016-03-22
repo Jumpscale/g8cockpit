e847cab508a553c10ab5ea2a1e3908ac44bbdf12
christophe de Carvalho Pereira Martincs <christophe.dcpm@gmail.com>
2016-03-22 12:23:35 +0400

    add upgrade commnand

--- scripts/building.py
+++ scripts/building.py
@@ -29,7 +29,7 @@ def update():
 
 @click.command()
 @click.option('--host', help='connection string to the host where to build the docker image', default='localhost')
-@click.option('--sshkey', help='Name of the ssh key to authorize on the G8Cockpit. key are fetch from ssh-agent.')
+@click.option('--sshkey', help='Name of the ssh key to authorize on the G8Cockpit. key are fetch from ssh-agent.',  default='id_rsa')
 def build(host, sshkey):
     """
     Build g8cockpit docker image
@@ -49,6 +49,7 @@ def build(host, sshkey):
     container.portal.install(start=False, minimal=True)
     container.builder.mongodb(start=False)
     container.builder.influxdb(start=False)
+    container.builder.grafana(start=False)
     container.run("js 'j.actions.resetAll()'")  # FIXME find why if we don't reset action before installing controller, everything explode
     container.builder.controller(start=False)
     container.builder.caddy(start=False)
@@ -61,8 +62,36 @@ def build(host, sshkey):
     container.dir_remove("$tmpDir/*")
     container.dir_remove("$varDir/data/*")
 
-    cuisine.run('jsdocker commit -n %s -t %s' % (container_name, image_name))
-    cuisine.run('jsdocker push -i %s' % image_name)
+    cuisine.core.run('jsdocker commit -n %s -t %s' % (container_name, image_name))
+    cuisine.core.run('jsdocker push -i %s' % image_name)
+
+
+@click.command()
+@click.option('--host', help='connection string to the host where to build the docker image', default='localhost')
+@click.option('--sshkey', help='Name of the ssh key to authorize on the G8Cockpit. key are fetch from ssh-agent.', default='id_rsa')
+def upgrade(host, sshkey):
+    """
+    upgrade the jumpscale/g8cockpit docker image.
+    Use this when you just want to update last code of jumpscale and not rebuild evrything from scratch
+    """
+    container_name = "cockpit_build"
+    image_name = "jumpscale/g8cockpit"
+
+    cuisine = j.tools.cuisine.get(host)
+
+    printInfo("create builder container")
+    key_pub = j.do.getSSHKeyFromAgentPub(sshkey)
+    container_conn_str = cuisine.docker.ubuntu(name=container_name, image='jumpscale/g8cockpit', ports="18384:18384", volumes=None, pubkey=key_pub, aydofs=False)
+    container = j.tools.cuisine.get(container_conn_str)
+    repos = [
+        'https://github.com/Jumpscale/ays_jumpscale8.git',
+        'https://github.com/Jumpscale/jumpscale_core8.git',
+        'https://github.com/Jumpscale/jumpscale_portal8.git',
+    ]
+    for url in repos:
+        j.do.pullGitRepo(url=url, executor=container.executor)
+    cuisine.core.run('jsdocker commit -n %s -t %s --force' % (container_name, image_name))
+    cuisine.core.run('jsdocker push -i %s' % image_name)
 
 def printErr(msg):
     msg = '[-]: %s' % msg
@@ -84,6 +113,7 @@ def exit(err, code=1):
 # Register Command
 cli.add_command(build)
 cli.add_command(update)
+cli.add_command(upgrade)
 
 if __name__ == '__main__':
     cli()
