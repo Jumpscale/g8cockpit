910f1cc9c30a5eb2520156bbfc4f445db9cfc750
christophe de Carvalho Pereira Martincs <christophe.dcpm@gmail.com>
2016-05-10 00:49:44 +0400

    rewrite webhooks endpoint.
    - now use json format from github

--- app/webhooks_api/webhooks.py
+++ app/webhooks_api/webhooks.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify, request
-# from JumpScale import j
+from JumpScale import j
 
 
 webhooks_api = Blueprint('webhooks_api', __name__)
@@ -11,9 +11,9 @@ def webhooks_github_post():
     endpoint that receives the events from github
     It is handler for POST /webhooks/github
     '''
-    if 'HTTP_X_GITHUB_EVENT' not in request.headers or 'HTTP_X_GITHUB_DELIVERY' not in request.headers:
+    if 'X-GitHub-Event' not in request.headers or 'X-GitHub-Delivery' not in request.headers:
         return '', 400
 
-    key = '%s.%s.%s' % (request.headers.get('HTTP_X_GITHUB_EVENT') , request.headers.get('HTTP_X_GITHUB_DELIVERY'), j.data.time.epoch)
-    j.core.db.hset('webhooks', key, request.data)
+    key = '%s.%s.%s' % (request.headers.get('X-GitHub-Event'), request.headers.get('X-GitHub-Delivery'), j.data.time.epoch)
+    j.core.db.hset('webhooks', key, j.data.serializer.json.dumps(request.json))
     return '', 201
