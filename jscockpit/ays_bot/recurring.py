e76cf1efc0197a8b3317cc5dcbe04eda8e2bf8bb
Muhamad Azmy <muhamad.azmy@codescalers.com>
2016-06-16 11:46:28 +0200

    Rework recurring (#38)

--- jscockpit/ays_bot/recurring.py
+++ jscockpit/ays_bot/recurring.py
@@ -23,15 +23,16 @@ class AysRecurring:
 
     def _recurring_loop(self):
         while self.bot.running is True:
-
-            now = j.data.time.epoch
             with self._load_repo_lock:
                 for service, actions in self._services_reccurring.items():
                     for action_name, info in actions.items():
+                        if 'period' not in info:
+                            continue
 
                         sec = j.data.time.getDeltaTime(info['period'])
-                        last = info['last']
+                        last = info.get('last', None)
 
+                        now = j.data.time.epoch
                         if last is None or now > (last + sec):
                             rq = self.bot.schedule_action(
                                 action=action_name,
@@ -39,7 +40,12 @@ class AysRecurring:
                                 role=service.role,
                                 instance=service.instance,
                                 force=True)
+
                             self._services_reccurring[service][action_name]['last'] = now
+                            service.state.recurring[action_name][1] = now
+                            service.state.changed = True
+                            service.state.save()
+
                             gevent.spawn(self.bot.handle_action_result, rq, action_name, service.aysrepo.name, service.role, service.instance)
 
             gevent.sleep(1)
@@ -59,8 +65,14 @@ class AysRecurring:
 
         for repo in repos:
             for service in repo.findServices():
-                for action_name, period in service.state.recurring.items():
-                    if service not in self._services_reccurring:
-                        self._services_reccurring[service] = {}
-                    self._services_reccurring[service][action_name] = {'period': period[0], 'last': None}
+                jobs = self._services_reccurring.setdefault(service, {})
+                keys = list(jobs.keys())
+                for action_name, state in service.state.recurring.items():
+                    job = jobs.setdefault(action_name, {})
+                    job.update(period=state[0])
+                    if action_name in keys:
+                        keys.remove(action_name)
                     self.bot.logger.debug("register service recurring from repo %s, service %s, action %s" % (repo.basepath, service.key, action_name))
+
+                for k in keys:
+                    jobs.pop(k, None)
