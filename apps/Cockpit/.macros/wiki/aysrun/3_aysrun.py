8570b77a0efa73e10d00f1058c4f2903affe2e75
Reem Khamis <khamisr@codescalers.com>
2016-06-27 17:21:23 +0200

    Add colored states to AYS runs

--- apps/Cockpit/.macros/wiki/aysrun/3_aysrun.py
+++ apps/Cockpit/.macros/wiki/aysrun/3_aysrun.py
@@ -6,6 +6,9 @@ def main(j, args, params, tags, tasklet):
 
     aysrun = j.apps.system.atyourservice.getRun(runid=arg_runid, repository=arg_repo, ctx=args.requestContext)
     aysrun['model']['time'] = j.data.time.epoch2HRDateTime(aysrun['model']['time'])
+    if aysrun['model']['state'] == 'ERROR':
+        action = [step['action'] for step in aysrun['model']['steps'] for action in step['actions'] if action['state'] == 'ERROR']
+        aysrun['model']['actionkey'] = 'actions.%s.__GUARD__' % action[0] if action else ''
     args.doc.applyTemplate(aysrun)
     params.result = (args.doc, args.doc)
     return params
