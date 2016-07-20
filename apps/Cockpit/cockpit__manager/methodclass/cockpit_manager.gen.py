41d627db3cd1d65a8077e0e0fc71639e79d1781c
christophe de Carvalho <christophe@incubaid.com>
2016-07-20 08:13:27 +0000

    add version page and update button

---
+++ apps/Cockpit/cockpit__manager/methodclass/cockpit_manager.gen.py
@@ -0,0 +1,22 @@
+from JumpScale import j
+
+class cockpit_manager(j.tools.code.classGetBase()):
+    """
+    cockpit actor
+    """
+    def __init__(self):
+        pass
+        
+        self._te={}
+        self.actorname="manager"
+        self.appname="cockpit"
+        #cockpit_manager_osis.__init__(self)
+
+
+    def update(self, **kwargs):
+        """
+        Update cockpit
+        result json
+        """
+        #put your code here to implement this method
+        raise NotImplementedError ("not implemented method update")
