6c6747ca0c7b9441cb214fb521a39751f5090563
Ahmed T. Youssef <xmonader@gmail.com>
2016-07-25 17:33:15 +0300

    fixes #141 (#158)

--- apps/Cockpit/.macros/wiki/aysruns/3_aysruns.py
+++ apps/Cockpit/.macros/wiki/aysruns/3_aysruns.py
@@ -1,23 +1,45 @@
 from collections import OrderedDict
 
 def main(j, args, params, tags, tasklet):
+    import jinja2
+    params.result = page = args.page
     doc = args.doc
     arg_repo = args.getTag('repo')
-    out = list()
+    klass = 'jstimestamp'
 
+
+    out = list()
     # this makes sure bootstrap datatables functionality is used
-    out.append("{{datatables_use}}\n")
+    out.append("{{datatables_use}}\n{{timestamp}}\n")
     for repo_path, runs in j.apps.cockpit.atyourservice.listRuns(repository=arg_repo, ctx=args.requestContext).items():
         out.append('h5. AYSRepo: %s' % repo_path)
-        out.append('|| Run ID || Ran At || State ||')
+        html = '''
+<table class="table table-striped table-bordered display JSdataTable dataTable">
+<thead>
+  <td>Run ID</td><td>Run AT</td><td>State</td>
+</thead>
+{% for run in runs %}
+<tr>
+  <td><a data-dummy="{{run.sortkey}}" href="cockpit/Run?repo={{repo_path}}&runid={{run.id}}">{{run.id}}</a></td>
+  <td><span class="jstimestamp" data-ts="{{run.runat}}"></span></td>
+  <td>{{run.state}}</td>
+</tr>
+{% endfor %}
+</table>
+        '''
+        template = jinja2.Template(html)
+        aysruns = []
+        for key, value in runs['aysruns'].items():
+            runat, state = value.split('|')
+            runid = int(key)
+            aysrun = {'id': runid,
+                      'state': state,
+                      'sortkey': '%05d' % runid,
+                      'runat': runat}
+            aysruns.append(aysrun)
 
-        runs = OrderedDict(sorted(runs['aysruns'].items()))
-        for runid, run_info in runs.items():
-            line = ['|[%s|cockpit/Run?repo=%s&runid=%s]' % (runid, repo_path, runid)]
-            time, state = run_info.split('|')
-            time = j.data.time.epoch2HRDateTime(time)
-            line.extend([time, state])
-            out.append("|".join(line) + '|')
+        table = "{{html:\n%s\n}}" % template.render(runs=sorted(aysruns, key=lambda x: x['id']), repo_path=repo_path)
+        out.append(table)
 
     out = '\n'.join(out)
     params.result = (out, doc)
