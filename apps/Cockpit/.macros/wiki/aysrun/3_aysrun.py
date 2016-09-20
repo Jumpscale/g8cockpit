41c94474755af6a132d034cb738426ab99966c57
Ashraf Fouda <foudaa@codescalers.com>
2016-09-20 15:43:40 +0200

    Adds don't crash when there is no errors  in the Run page
    Signed-off-by: Ashraf Fouda <foudaa@codescalers.com>

--- apps/Cockpit/.macros/wiki/aysrun/3_aysrun.py
+++ apps/Cockpit/.macros/wiki/aysrun/3_aysrun.py
@@ -10,7 +10,7 @@ def main(j, args, params, tags, tasklet):
         if aysrun['model']['state'] == 'ERROR':
             action = [step['action'] for step in aysrun['model']['steps'] for action in step['actions'] if action['state'] == 'ERROR']
             aysrun['model']['actionkey'] = 'actions.%s.__GUARD__' % action[0] if action else ''
-            args.doc.applyTemplate(aysrun)
+        args.doc.applyTemplate(aysrun)
     except Exception as e:
         args.doc.applyTemplate({'error': str(e)})
 
